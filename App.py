9842d16eee66a6917115ec495b4c9c78f16677dc
SuvanD0 <suvandommeti0@gmail.com>
2023-02-22 23:23:35 -0500

    updated layout

--- App.py
+++ App.py
@@ -105,8 +105,8 @@ else:
         dist_data['trip_distance_km'] = dist_data['trip_distance_miles']*1.60934
         fig = px.bar(dist_data, x='weekday', y='trip_distance_km', labels={'trip_distance_km':'Distance Traveled (km)'})
         fig.update_layout(
-            width=900,
-            height=500,
+            width=500,
+            height=300,
         )
         st.plotly_chart(fig)
 
